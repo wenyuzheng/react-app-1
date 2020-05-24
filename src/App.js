8dc34946cc2df0f0ad856f95c7f4bde8b1c20b96
Wenyu Zheng <zhengwy229@gmail.com>
2020-05-24 10:27:32 +0100

    test: show two lines

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>hello</h1>
+        <p> this is working</p> 
+      </div>
+
+      // This doesn't work because can only render one root component so it has to be inside 'div'
+      //<p> this is working</p> 
+    );
+
+    // This works but not recommended when there are many nested components.
+    // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Hi!!!!'));
+  }
 }
 
 export default App;
