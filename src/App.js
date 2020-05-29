43bdde3969463920e8381d068e3ea00434b38bd4
Wenyu Zheng <zhengwy229@gmail.com>
2020-05-29 10:18:18 +0100

    add textbox to change name and add css

--- src/App.js
+++ src/App.js
@@ -12,28 +12,56 @@ class App extends Component{
     otherState: 'Other values'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // Should not change state directly like this, React can't recgonised it.
     // DON'T DO THIS: this.state.persons[0].name = 'Daisy';
 
     this.setState({
       persons: [
-        { name: "Daisy", age: 16 },
+        { name: newName, age: 16 },
         { name: "Bob", age: 20 },
         { name: "Calvin", age: 9 }
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Amy", age: 16 },
+        { name: event.target.value, age: 20 },
+        { name: "Calvin", age: 9 }
+      ]
+    })
+  }
+
   render(){
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border : '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>hello</h1>
         <p> this is working</p> 
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Hobbies: Racing </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler("daisyyy")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Dave")}
+          changed={this.nameChangedHandler}> Hobbies: Racing </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
 
       // This doesn't work because can only render one root component so it has to be inside 'div'
