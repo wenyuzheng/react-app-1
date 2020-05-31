53d9cbd9bc11bd29cab74827e30e87c05f22c9d0
Wenyu Zheng <zhengwy229@gmail.com>
2020-05-31 07:54:52 +0100

    toggle persons and delete person

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Person from './Person/Person';
 class App extends Component{
   state = {
     persons: [
-      { name: "Amy", age: 16 },
-      { name: "Bob", age: 20 },
-      { name:"Calvin", age: 9 }
+      { id: 'p1', name: "Amy", age: 16 },
+      { id: 'p2', name: "Bob", age: 20 },
+      { id: 'p3', name:"Calvin", age: 9 }
     ],
-    otherState: 'Other values'
+    otherState: 'Other values',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -25,23 +26,65 @@ class App extends Component{
     })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Amy", age: 16 },
-        { name: event.target.value, age: 20 },
-        { name: "Calvin", age: 9 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    // NOT GOOD: const person = this.state.persons[personIndex];
+    const person = { ...this.state.persons[personIndex] }; // OR const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // JS objects are reference types so should not mutate them directly (i.e change the original state).
+    // Instead we should create a copy of the original state and change that copy.
+    // so NOT GOOD: const newPersons = this.state.persons;
+
+    const newPersons = [...this.state.persons]; // OR const newPersons = this.state.persons.slice();
+    newPersons.splice(personIndex, 1);  //To remove 1 element from the array
+    this.setState({persons: newPersons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render(){
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border : '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      margin: '20px'
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              name={person.name} 
+              age={person.age}
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+             })}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
     }
 
     return (
@@ -49,19 +92,26 @@ class App extends Component{
         <h1>hello</h1>
         <p> this is working</p> 
         <button 
-        style={style}
-        onClick={() => this.switchNameHandler("daisyyy")}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Dave")}
-          changed={this.nameChangedHandler}> Hobbies: Racing </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+
+        {/* Another way to hide/show persons: using ternary operation */}
+          {/* this.state.showPersons ? 
+            <div>
+              <Person 
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age} />
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, "Dave")} //Or use a function: {() => this.switchNameHandler("daisyyy")}
+                changed={this.nameChangedHandler}> Hobbies: Racing </Person>
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age} />
+            </div> : null */}
+
       </div>
 
       // This doesn't work because can only render one root component so it has to be inside 'div'
